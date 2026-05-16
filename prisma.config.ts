2316e8a2fe1a967d0a2278e5946afccdc9b1f38d
Prahlad Sharma <79293553+CoderPrahlad@users.noreply.github.com>
2026-05-16 08:08:18 +0530

    Delete prisma.config.ts

--- prisma.config.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path'
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  datasourceUrl: process.env.DATABASE_URL ?? `file:${path.join(process.cwd(), 'db/custom.db')}`,
-})
